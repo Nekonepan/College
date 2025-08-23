356b24a962521459dfbdde4527871f3869fd1582
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-08-23 20:45:05 +0700

    feat: fix array declaration and initialization for min and max calculations

--- C++/Miscellaneous/menentukan-bilangan-tekecil-terbesar.cpp
+++ C++/Miscellaneous/menentukan-bilangan-tekecil-terbesar.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main() {
-    int arr[][] = {
+    int arr[2][3] = {
         {12, 7, 28}, 
-        {4, 10, 99};
-    }
+        {4, 10, 99}
+    };
 
     // inisialisasi min dan max dengan elemen pertama
-    int min = arr[0];
-    int max = arr[0];
+    int min = arr[0][0];
+    int max = arr[0][0];
 
-    // cek semua elemen mulai dari indeks ke-1
+    // cek semua elemen
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
-            if (arr[i] < min) {
-                min = arr[i]; // update min jika ketemu lebih kecil
+            if (arr[i][j] < min) {
+                min = arr[i][j]; // update min jika ketemu lebih kecil
             }
-            if (arr[i] > max) {
-                max = arr[i]; // update max jika ketemu lebih besar
+            if (arr[i][j] > max) {
+                max = arr[i][j]; // update max jika ketemu lebih besar
             }
         }
     }
