9291cf59e047f77946b7c96a55795073358b2541
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-09-25 12:35:49 +0700

    feat: refactor main function to calculate average, min, max, and count students with scores above 70

--- C++/Miscellaneous/olah-nilai.cpp
+++ C++/Miscellaneous/olah-nilai.cpp
@@ -4,22 +4,48 @@
 #include <iostream>
 using namespace std;
 
-void hitung_rata_rata() {
-  float n, nilai, rata_rata;
+int main() {
+  int n;
   float jumlah = 0;
+  float rata_rata = 0;
+
   cout << "Masukkan jumlah mahasiswa : ";
   cin >> n;
 
+  float* arr = new float[n];
+
   for (int i = 0; i < n; i++) {
     cout << "Masukkan nilai mahasiswa ke-" << i << " : ";
-    cin >> nilai;
-    jumlah += nilai;
+    cin >> arr[i];
   }
 
-  rata_rata = jumlah / n;
-  cout << "Rata-rata : " << rata_rata;
-}
+  for (int i = 0; i < n; i++) {
+    jumlah += arr[i];
+    rata_rata = jumlah / n;
+  }
 
-int main() {
-  hitung_rata_rata();
+  float min = arr[0];
+  float max = arr[0];
+
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  int counter = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] > 70) {
+      counter++;
+    }
+  }
+
+  cout << "Nilai rata-rata mahasiswa = " << rata_rata << endl;
+  cout << "Nilai terkecil mahasiswa = " << min << endl;
+  cout << "Nilai terbesar mahasiswa = " << max << endl;
+  cout << "Jumlah mahasiswa yang mendapat nilai lebih dari 70 = " << counter << endl;
+  
 }
